45e54b65a682371efbb06e2bda1e6e91547db5df
orange2120 <orange21201@gmail.com>
2019-08-14 14:10:17 +0800

    0814 updated

--- button_tone/button_tone.ino
+++ button_tone/button_tone.ino
@@ -1,25 +1,25 @@
-/*
- * Melody based lock door
+/* 
+ * Melody based door lock
+ * if you press right melody, the door will unlock.
  */
 #include <Bounce2.h>
 
 const uint8_t button_pin[] = {4, 5, 6, 7, 8, 9, 10};
 const uint8_t lock_pin = 11;
 
-// tone of the song
-#define nBEATS 13
-const uint8_t tones[] = {1,1,5,5,6,6,5,4,4,3,3,2,2,1}; // little star
-
-volatile bool record[nBEATS] = {0};
-int current_beat = 0;
-
 #define nButton 7 // number of buttons
-
 Bounce *sw[7] = {NULL};
+
+#define DOOR_OPEN_DELAY 10000 //ms
+
+// tone of the song
+#define nBEATS 14
+const uint8_t tones[] = {1,1,5,5,6,6,5,4,4,3,3,2,2,1}; // little star
+volatile uint8_t record[nBEATS] = {0};
 
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
 
     for (uint8_t i = 0; i < nButton; ++i)
         sw[i] = new Bounce;
@@ -31,6 +31,7 @@ void setup()
     }
 
     pinMode(lock_pin, OUTPUT);
+    digitalWrite(lock_pin, LOW);
 
     resetRecord();
     Serial.println("done.");
@@ -41,19 +42,25 @@ void loop()
     // update button status
     for (uint8_t i = 0; i < nButton; ++i)
         sw[i]->update();
-
     for (uint8_t i = 0; i < nButton; ++i)
     {
         if(sw[i]->fell()) // i th botton be pressed
         {
-            Serial.println(i);
-            record_fifo(i);
+            record_fifo(i + 1);
+            
+            #ifdef DEBUG
+            for (uint8_t j = 0; j < nBEATS; ++j)
+                Serial.print(record[j]);
+
+                Serial.println("");
+            #endif
         }
     }
 
+    // check the tones is correspond with answer
     for (uint8_t i = 0; i < nBEATS; ++i)
     {
-        if (record[i] != record[i])
+        if (record[i] != tones[i])
             break;
         if (i == (nBEATS - 1)) // all the tones are the same
             unlock();
@@ -66,7 +73,7 @@ void record_fifo(uint8_t k)
 {
     for (uint8_t i = 0; i < nBEATS - 1; ++i)
     {
-        record[i] = record[i + i]; // move forward(pop first element)
+        record[i] = record[i + 1]; // move forward(pop first element)
     }
     record[nBEATS - 1] = k; // push back
 }
@@ -74,20 +81,17 @@ void record_fifo(uint8_t k)
 void resetRecord()
 {
 	for (uint8_t i = 0; i < nBEATS; ++i)
-	{
 		record[i] = 0;
-	}
 }
 
+// unlock the door
 void unlock()
 {
-    Serial.println("UNLK");
+    #ifdef DEBUG
+        Serial.println("UNLK");
+    #endif
+    digitalWrite(lock_pin, HIGH);
+    delay(DOOR_OPEN_DELAY);
+    digitalWrite(lock_pin, LOW);
+    resetRecord();
 }
-
-// 方案
-/*
-一次紀錄14個 
-這樣無法從中間開始
-動態檢查
-這個可以
- */
